f4803d26d3abc18ce160f2fc3bb83200c1da68c6
trongpv <trongpv@unitech.vn>
2024-01-02 23:44:36 +0900

    Add Error handling strategy

---
+++ BubberDinner.Domain/Common/Errors/Errors.User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Domain.Common.Errors
+{
+    public static class Errors
+    {
+        public static class User
+        {
+            public static Error DulicateEmail => Error.Conflict(code: "User.DuplicateEmail", DescriptionAttribute: "Email is already in use.");
+        }
+    }
+}
